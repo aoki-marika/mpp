27c0833254574a34f2960d3c592c5e6b17eef5ab
marika <marika@waifu.club>
2019-01-08 19:29:42 -0400

    Replace config.yml with a database.yml that can be read by sequel.

--- routes/app.rb
+++ routes/app.rb
@@ -1,13 +1,19 @@
+require 'yaml'
+require 'sequel'
 require 'sinatra/base'
 
 require_relative 'base.rb'
-require_relative '../models/config.rb'
 require_relative '../utilities/filesystem.rb'
 
-# config is required to load the db, and the db is required to require model files
-CONFIG = Model::Config.new(Utilities::Filesystem.absolute_path(__FILE__, '../config.yml'));
-DB = Sequel.sqlite(CONFIG.environments[ENV['RACK_ENV']].db);
+# get the db path
+db_config_path = Utilities::Filesystem.absolute_path(__FILE__, '../config/database.yml'); # the path for the db config file
+db_config = YAML.load_file(db_config_path)[ENV['RACK_ENV']]; # the db config for the current environment
+db_base_path = Utilities::Filesystem.absolute_path(__FILE__, '../db/'); # the base path for dbs
 
+# connect to the db
+DB = Sequel.connect("#{db_config['adapter']}://#{File.join(db_base_path, db_config['database'])}");
+
+# require models now that the db is connected
 require_relative '../models/user.rb'
 
 module Routes
