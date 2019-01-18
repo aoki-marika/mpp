8a05449d325224c3226dcac03b4e69e4bd4b229c
marika <marika@waifu.club>
2019-01-18 15:12:09 -0400

    Use `:index => true` instead of `index [...]` in migrations.

--- db/migrations/20190117143524_paths.rb
+++ db/migrations/20190117143524_paths.rb
@@ -2,10 +2,8 @@ Sequel.migration do
     change do
         create_table(:paths) do
             primary_key :id
-            foreign_key :series_id, :series, :default => nil
+            foreign_key :series_id, :series, :index => true, :default => nil
             String :path, :size => 4096, :null => false
-
-            index [:series_id]
         end
     end
 end
