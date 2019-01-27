033821c63118f3c761b52e7d85f3eec8b9851e60
marika <marika@waifu.club>
2019-01-26 20:33:45 -0400

    Add Madokami.get_pages for scraping pages of an archive.

--- app/utils/madokami.rb
+++ app/utils/madokami.rb
@@ -1,5 +1,8 @@
 require 'net/http'
 require 'uri'
+require 'erb'
+
+require_relative '../models/errors.rb'
 
 class Madokami
     # Make a request to the given path on Madokami with the given credentials.
@@ -24,6 +27,31 @@ class Madokami
 
             # make the request and yield the response
             yield http.request(request)
+        end
+    end
+
+    # Make a request to get the pages for an archive.
+    def self.get_pages(archive_path, token)
+        # for whatever reason madokami double encodes paths in urls
+        path = "/reader/#{ERB::Util.url_encode(ERB::Util.url_encode(archive_path))}"
+
+        # make the reader request
+        self.request path, token: token do |r|
+            case r.code
+                when '200'
+                    # parse out the files array
+                    files = r.body.match(/(?<=data-files="\[).+?(?=\]")/)[0].split(',')
+
+                    # cleanup all the filenames and map them to an array of hashes
+                    files.map.with_index do |f, i|
+                        f.gsub! '&quot;', ''
+                        f.gsub! '\\/', '/'
+
+                        { :path => f, :index => i }
+                    end
+                else
+                    raise UnauthorizedError.new
+            end
         end
     end
 end
